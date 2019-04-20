7c168b8a4472b0719b05f3143b77db3920fc24ab
Dylan Stein <djstein@ncsu.edu>
2019-04-20 12:11:30 -0700

    add checklists post

--- src/blog/constants/entries.js
+++ src/blog/constants/entries.js
@@ -1,8 +1,14 @@
 export const entries = {
+  checklists: {
+    title: 'Checklists',
+    subtitle: 'Thoughts on a checklist app',
+    publishDate: '4/20/2019',
+    featured: true,
+  },
   'new-website': {
     title: 'New Website',
     subtitle: 'The new website',
-    publishDate: '2/15/2018',
+    publishDate: '2/15/2019',
     featured: true,
   },
 }
